1bbc00f8c3d486ab0a7239a268b1487074ca3a03
brock <laoyaogg@qq.com>
2014-11-17 13:49:48 +0800

    changed

--- webscanner.py
+++ webscanner.py
@@ -484,6 +484,7 @@ if __name__ == "__main__":
 	
 	reload(sys)
 	sys.setdefaultencoding(locale.getpreferredencoding())
+
 	opts, args = getopt.getopt(sys.argv[1:], "ad:e:f:hk:n:N:st:vw:")
 	#print opts
 	#print args
