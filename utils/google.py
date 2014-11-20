bf1ce90118ff6af9fbe457ebdbb3c914acd65123
Brock <8274634@@qq.com>
2014-11-20 05:19:01 -0500

    changed

--- utils/google.py
+++ utils/google.py
@@ -1,12 +1,28 @@
+import os
 import gfsoso
 import aolsearch
 import googlesearch
 import bingsearch
 
-searchEngine = gfsoso.google
 #searchEngine = googlesearch.google
 #searchEngine = aolsearch.google
 #searchEngine = bingsearch.google
 
-google = searchEngine
+if os.environ.has_key('search_engine'):
+	search_engine = os.environ['http_proxy']
+else:
+	search_engine = 'gfsoso'
+
+if search_engine == 'gfsoso':
+	google = gfsoso.google
+elif search_engine == 'google':
+ 	google = googlesearch.google
+elif search_engine == 'aol':
+ 	google = aolsearch.google
+elif search_engine == 'bing':
+ 	google = gingsearch.google
+else:
+	google = searchEngine
+
+searchEngine = google
 
